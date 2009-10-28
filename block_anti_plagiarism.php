48e99abbfea36a94809feda7c3b012d66abbb8f9
Sun Zhigang <sunner@gmail.com>
2009-10-28 12:57:57 +0000

    Add copyright

--- block_anti_plagiarism.php
+++ block_anti_plagiarism.php
@@ -1,4 +1,26 @@
-<?PHP //$Id: block_anti_plagiarism.php,v 1.6 2004/10/03 09:50:39 stronk7 Exp $
+<?PHP
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// NOTICE OF COPYRIGHT                                                   //
+//                                                                       //
+//            Online Judge assignment type for Moodle                    //
+//           http://code.google.com/p/sunner-projects/                   //
+//                                                                       //
+// Copyright (C) 2009 onwards  Sun Zhigang  http://sunner.cn             //
+//                                                                       //
+// This program is free software; you can redistribute it and/or modify  //
+// it under the terms of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 3 of the License, or     //
+// (at your option) any later version.                                   //
+//                                                                       //
+// This program is distributed in the hope that it will be useful,       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+// GNU General Public License for more details:                          //
+//                                                                       //
+//          http://www.gnu.org/copyleft/gpl.html                         //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
 
 class block_anti_plagiarism extends block_list {
 
--- view.php
+++ view.php
@@ -1,4 +1,27 @@
 <?php
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// NOTICE OF COPYRIGHT                                                   //
+//                                                                       //
+//            Online Judge assignment type for Moodle                    //
+//           http://code.google.com/p/sunner-projects/                   //
+//                                                                       //
+// Copyright (C) 2009 onwards  Sun Zhigang  http://sunner.cn             //
+//                                                                       //
+// This program is free software; you can redistribute it and/or modify  //
+// it under the terms of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 3 of the License, or     //
+// (at your option) any later version.                                   //
+//                                                                       //
+// This program is distributed in the hope that it will be useful,       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+// GNU General Public License for more details:                          //
+//                                                                       //
+//          http://www.gnu.org/copyleft/gpl.html                         //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+
 
 require_once('../../config.php');
 require_once('../../lib/filelib.php'); // for get_file_url() and fulldelete()
