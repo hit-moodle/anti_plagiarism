ba3168ce6e94e88435ec870577b3dacb596bbcc7
Sun Zhigang <sunner@gmail.com>
2009-11-24 11:58:23 +0000

    Better input type

--- settings.php
+++ settings.php
@@ -1,11 +1,11 @@
 <?php
 
-$settings->add(new admin_setting_configtext('block_antipla_duplication_path', get_string('duplicationpath', 'block_anti_plagiarism'),
-                   get_string('describeduplication', 'block_anti_plagiarism'), 128, PARAM_PATH));
+$settings->add(new admin_setting_configexecutable('block_antipla_duplication_path', get_string('duplicationpath', 'block_anti_plagiarism'),
+                   get_string('describeduplication', 'block_anti_plagiarism'), ''));
 
-$settings->add(new admin_setting_configtext('block_antipla_moss_script_path', get_string('mossscriptpath', 'block_anti_plagiarism'),
-                   get_string('describemoss', 'block_anti_plagiarism'), 128, PARAM_PATH));
+$settings->add(new admin_setting_configexecutable('block_antipla_moss_script_path', get_string('mossscriptpath', 'block_anti_plagiarism'),
+                   get_string('describemoss', 'block_anti_plagiarism'), ''));
 
-$settings->add(new admin_setting_configtext('block_antipla_unrar_path', get_string('unrarpath', 'block_anti_plagiarism'),
-                   get_string('describeunrar', 'block_anti_plagiarism'), 128, PARAM_PATH));
+$settings->add(new admin_setting_configexecutable('block_antipla_unrar_path', get_string('unrarpath', 'block_anti_plagiarism'),
+                   get_string('describeunrar', 'block_anti_plagiarism'), ''));
 ?>
